c6cf2a18aac040f28a5d5685ac3975764e6e5ee3
jaime <garciadiazjaime@gmail.com>
2021-06-11 22:58:36 -0500

    feat: unzip

--- unzip.py
+++ unzip.py
@@ -1,23 +1,33 @@
-from os import listdir
+from os import listdir, makedirs
 from os.path import isfile, join
-
 from zipfile import ZipFile
 
 
 def get_zip_files(path):
-  files = [f for f in listdir(path) if isfile(join(path, f))]
+  files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.zip')]
   return files
 
-def unzip_file(path):
-  with ZipFile(path, 'r') as zipObj:
-    zipObj.extractall()
+def check_folder(path):
+  makedirs(path, exist_ok=True)
+
+def unzip_file(path_from, file, path_to):
+  zipped_path = path_from + "/" + file
+  with ZipFile(zipped_path, 'r') as zipObj:
+    [name, _] = file.split(".")
+    unzip_path = path_to + "/" + name
+    check_folder(unzip_path)
+    print("unziping: %s into %s" % (zipped_path, unzip_path))
+    zipObj.extractall(unzip_path)
 
 def main():
-  path = "./aviation/airline_origin_destination"
-  files = get_zip_files(path)
+  path_from = "./aviation/airline_origin_destination"
+  path_to = "./data/airline_origin_destination"
+  files = get_zip_files(path_from)
+
+  check_folder(path_to)
 
   for file in files:
-    unzip_file(path + '/' + file)
+    unzip_file(path_from, file, path_to)
 
 if __name__ == "__main__":
   main()
